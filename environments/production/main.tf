2c8721a34b762eaafef2d36c1c6311a8f5cfefbb
Jeff Sushetski <jsushetski@gmail.com>
2020-01-17 16:14:44 -0700

    Fixed environment names for production and staging.

--- environments/production/main.tf
+++ environments/production/main.tf
@@ -1,6 +1,6 @@
 module "production-elasticache" {
   source             = "../../elasticache"
-  environment        = "dev"
+  environment        = "production"
   node_count         = 3
   node_type          = "cache.m3.large"
   availability_zones = ["us-east-1a", "us-east-1b"]
